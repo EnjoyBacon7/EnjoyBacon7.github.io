8a05be87c9ad6cb348ff1c9d04f963827f6b06fe
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2022-01-19 12:15:54 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const parallax = document.getElementsByClassName("parallax");
+const bodyRect = document.body.getBoundingClientRect();
+
+const initialPos = [];
+for (var i = 0; i < parallax.length; i++) {
+    var elementRect = parallax[i].getBoundingClientRect();
+    initialPos[i] = elementRect.top - bodyRect.top;
+}
+
+window.addEventListener("scroll", function()
+{
+    let offset = window.pageYOffset;
+    console.log(window.pageYOffset);
+    for (var i = 0; i < parallax.length; i++) {
+        parallax[i].style.backgroundPositionY = (initialPos[i]*0.7 + offset * (-0.7)) + "px";
+    }
+})
